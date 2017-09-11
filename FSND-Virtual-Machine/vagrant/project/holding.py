4276d8af8894c8d77ee6fc8cd88008097f44fd11
Steven Li <steven4354@gmail.com>
2017-09-11 14:35:03 -0700

    feat: found most top articles by visits

---
+++ FSND-Virtual-Machine/vagrant/project/holding.py
@@ -0,0 +1,20 @@
+        when path like 'bad.things.gone' then 'Bad things gone, say good people'
+        when path like 'balloon.goons.doomed' then 'Balloon goons doomed'
+        when path like 'bears.love.berries' then 'Bears love berries, alleges bear'
+        when path like 'candidate.is.jerk' then 'Candidate is jerk, alleges rival'
+        when path like 'goats.eat.google' then 'Goats eat Google's lawn'
+        when path like 'media.obsessed.with.bears' then 'Media obsessed with bears'
+        when path like 'so.many.bears' then 'There are a lot of bears'
+
+                when path like 'bad.things.gone' then 'Bad things gone, say good people'
+                when path like 'balloon.goons.doomed' then 'Balloon goons doomed'
+                when path like 'bears.love.berries' then 'Bears love berries, alleges bear'
+                when path like 'candidate.is.jerk' then 'Candidate is jerk, alleges rival'
+                when path like 'goats.eat.google' then 'Goats eat Google's lawn'
+                when path like 'media.obsessed.with.bears' then 'Media obsessed with bears'
+                when path like 'so.many.bears' then 'There are a lot of bears'
+
+                    end
+        case
+
+        when path like 'trouble.for.trouble' then 'Trouble for troubled troublemakers'
--- FSND-Virtual-Machine/vagrant/project/test.py
+++ FSND-Virtual-Machine/vagrant/project/test.py
@@ -3,8 +3,26 @@ import psycopg2
 def output():
     a = psycopg2.connect(database="news")
     b = a.cursor()
-    b.execute("select * from articles")
+    b.execute('''
+    select *,
+        case
+        when path like 'trouble.for.trouble' then 'Trouble for troubled troublemakers'
+        end
+    group by
+        path
+    from orderedpath
+    ;
+    ''')
     print(b.fetchall())
     a.close()
 
 output()
+
+ Bad things gone, say good people
+ Balloon goons doomed
+ Bears love berries, alleges bear
+ Candidate is jerk, alleges rival
+ Goats eat Google's lawn
+ Media obsessed with bears
+ Trouble for troubled troublemakers
+ There are a lot of bears
